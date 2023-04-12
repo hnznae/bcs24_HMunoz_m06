8d3eadc8e61eee9ed8431875095b13f388c956a0
Hanzen Nikolai R. Munoz <hanzen.munoz@gmail.com>
2023-04-12 11:27:55 +0800

    includes select control activity.

---
+++ index.js
@@ -0,0 +1,14 @@
+function oddEvenChecker(number) {
+	typeof(number) === 'number' ? number % 2 === 0 ? console.log("The number is even.") : console.log("The number is odd.") : console.log("Number is invalid.")
+	
+}
+
+oddEvenChecker(10);
+oddEvenChecker(11);
+oddEvenChecker("A");
+
+function budgetChecker(amount) {
+	typeof(amount) === 'number' ? amount > 40000 ? console.log("You are over budget") : console.log("You have resources left.") : console.log("Input is invalid.")
+}
+
+budgetChecker(10000);
